6f7266825a38abdf3ae8ea53fe610f8ae8c57296
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-29 17:42:14 +0300

    adding parseArguments function to pass arguments from cli using process.argv

--- bmicalculator.ts
+++ bmicalculator.ts
@@ -1,5 +1,23 @@
 type Result='Normal (healthy weight)'|'Underweight (unhealthy weight)'|'Overweight (unhealthy weight)';
 
+interface ParameterValues{
+  height:number; //in cm
+  weight:number; //kg
+}
+
+const parseArguments=(args:string[]):ParameterValues=>{
+   if(args.length>4) throw new Error('too many arguments')
+   if(args.length<4) throw new Error('Not enough arguments')
+   if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))){
+      return {
+        height:Number(args[2]),
+        weight:Number(args[3])
+      }
+   }else{
+    throw new Error('provided values were not number')
+   }
+}
+
 const bmiCalculator=(height:number,weight:number):Result=>{
   height=height/100
   const bmi=weight/(height*height)
@@ -11,8 +29,11 @@ const bmiCalculator=(height:number,weight:number):Result=>{
   else
   return 'Normal (healthy weight)' 
 }
+
 try {
-  console.log(bmiCalculator(170,80));
+  const {height, weight }=parseArguments(process.argv)
+  console.log(bmiCalculator(height,weight));
+  
 } catch (error:unknown) {
   if(error instanceof Error)
   console.log(`Error happened :Error :${error.message}`);
