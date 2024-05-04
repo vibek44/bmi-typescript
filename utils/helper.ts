50aaf02d461159fe0added7c1d747eabc5614d56
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-04 21:44:18 +0300

    adding type string to function arguments types

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,6 +1,6 @@
-export const isNotNumber=(args:any):number=>{
+export const isNotNumber=(args:string|number):number=>{
   if(isNaN(Number(args)))
-  throw new Error('arguments are not numbers')
- return (Number(args))
-}
+  throw new Error('arguments are not numbers');
+ return (Number(args));
+};
 
