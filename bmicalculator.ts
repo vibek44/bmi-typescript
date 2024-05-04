c5aeec50ed23148e89472ec0e6c5b72d30304cdd
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-04 21:39:51 +0300

    exporting bmiCalculator function and commenting out functionality needed for parsing cli arguments

--- bmicalculator.ts
+++ bmicalculator.ts
@@ -1,13 +1,28 @@
 type Result='Normal (healthy weight)'|'Underweight (unhealthy weight)'|'Overweight (unhealthy weight)';
 
+
+
+export const bmiCalculator=(height:number,weight:number):Result=>{
+  height=height/100;
+  const bmi=weight/(height*height);
+  console.log();
+  if(bmi>25)
+    return 'Overweight (unhealthy weight)';
+  else if(bmi<16)
+    return 'Underweight (unhealthy weight)';
+  else
+    return 'Normal (healthy weight)'; 
+};
+
+/*
 interface ParameterValues{
   height:number; //in cm
   weight:number; //kg
 }
 
 const parseArguments=(args:string[]):ParameterValues=>{
-   if(args.length>4) throw new Error('too many arguments')
-   if(args.length<4) throw new Error('Not enough arguments')
+   if( args.length > 4 ) throw new Error('too many arguments')
+   if( args.length < 4 ) throw new Error('Not enough arguments')
    if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))){
       return {
         height:Number(args[2]),
@@ -17,19 +32,6 @@ const parseArguments=(args:string[]):ParameterValues=>{
     throw new Error('provided values were not number')
    }
 }
-
-const bmiCalculator=(height:number,weight:number):Result=>{
-  height=height/100
-  const bmi=weight/(height*height)
-  console.log();
-  if(bmi>25)
-  return 'Overweight (unhealthy weight)'
-  else if(bmi<16)
-    return 'Underweight (unhealthy weight)'
-  else
-  return 'Normal (healthy weight)' 
-}
-
 try {
   const {height, weight }=parseArguments(process.argv)
   console.log(bmiCalculator(height,weight));
@@ -39,5 +41,5 @@ try {
   console.log(`Error happened :Error :${error.message}`);
   
 }
-
+*/
 
