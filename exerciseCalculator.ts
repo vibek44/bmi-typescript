db1b9c00fe0551345573169508cbd70c1bf2f3cc
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-04 21:40:24 +0300

    exporting exerciseCalculator function and commenting out functionality needed for parsing cli arguments

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,4 +1,3 @@
-import { isNotNumber } from "./utils/helper";
 
 interface exerciseResult {
   periodLength:number;
@@ -10,41 +9,43 @@ interface exerciseResult {
   average:number;
 }
 
-interface ReturnParseValues{
-  trainingHours:number[];
-  target:number
-}
 
-const parseArguments1=(args:string[]):ReturnParseValues=>{
- 
-  if( args.length < 4 ) throw new Error('Not enough arguments')
-   const [, , ...rest]=args
-   const argument  =rest.map(el=>isNotNumber(el))
-   const [target, ...trainingHours]=argument
-   return{
-    trainingHours,
-    target
-   }
-     
-}
 
-const exerciseCalculator=(traingHours:number[],target:number):exerciseResult=>{
- let sum=traingHours.reduce((sum,currentElement)=>sum+currentElement)
- let average=sum/traingHours.length
+export const exerciseCalculator=(trainingHours:number[],target:number):exerciseResult=>{
+ const sum=trainingHours.reduce((sum,currentElement)=>sum+currentElement);
+ const average=sum/trainingHours.length;
  return {
-  periodLength:traingHours.length,
-  trainingDays:traingHours.filter((hour)=>hour!==0).length,
+  periodLength:trainingHours.length,
+  trainingDays:trainingHours.filter((hour)=>hour!==0).length,
   success:average>=target,
   rating:average>=target?3:2,
   ratingDescription:average>=target?'keep going great with consistent achievement':'not too bad try again',
   target,
   average
- }
+ };
+};
+
+/*
+interface ReturnParseValues{
+  trainingHours:number[];
+  target:number;
 }
 
+const parseArguments1=(args:string[]):ReturnParseValues=>{
+ 
+  if( args.length < 4 ) throw new Error('Not enough arguments');
+   const [, , ...rest]=args;
+   const argument  =rest.map(el=>isNotNumber(el));
+   const [target, ...trainingHours]=argument;
+   return{
+    trainingHours,
+    target
+   };
+     
+};
 try {
   //let [ , , ...rest]=process.argv 
-  const{trainingHours,target}=parseArguments1(process.argv)
+  const{trainingHours,target}=parseArguments1(process.argv);
   //console.log(typeof target);
   console.log(exerciseCalculator(trainingHours,target));
 } catch (error:unknown) {
@@ -52,5 +53,5 @@ try {
   console.log(`Error : ${error.message}`);
   
 }
-
+*/
 
