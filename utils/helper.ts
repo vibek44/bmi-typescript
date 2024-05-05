a4443cdcecdc7ac144bb54611b6b791646ea0552
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-05 17:58:53 +0300

    adding error handling if condition

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,4 +1,5 @@
 export const isNotNumber=(args:string|number):number=>{
+  if(!args && args!==0) throw new Error('parameter is missing');
   if(isNaN(Number(args)))
   throw new Error('arguments are not numbers');
  return (Number(args));
